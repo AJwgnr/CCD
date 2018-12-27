e55a957419e2f6a763a3c731091591f7aea8fd3e
Fabian Rodriguez <fabianrbz@gmail.com>
2018-12-27 17:49:58 -0300

    Fix diagonal moves.

--- src/test/java/com/ovgu/ccd/moves/three/FullBoardTest.java
+++ src/test/java/com/ovgu/ccd/moves/three/FullBoardTest.java
@@ -17,10 +17,11 @@ public class FullBoardTest {
         ThreePlayerChessboard board = new ThreePlayerChessboard();
         board.initPlayers();
         board.initPieceStartPositions();
+        ArrayList<Square> moves = new ArrayList<>();
 
         Bishop rightWhiteBishop = (Bishop) board.getSquare(0, ThreePlayerChessboard.F).getPiece();
 
-        ArrayList<Square> moves = rightWhiteBishop.allMoves();
+        moves = rightWhiteBishop.allMoves();
         assertTrue(moves.size() == 0);
 
         Pawn whitePawn = (Pawn) board.getSquare(1, ThreePlayerChessboard.E).getPiece();
@@ -28,5 +29,31 @@ public class FullBoardTest {
 
         moves = rightWhiteBishop.allMoves();
         assertTrue(moves.size() == 5);
+
+        whitePawn = (Pawn) board.getSquare(1, ThreePlayerChessboard.D).getPiece();
+        board.setPiece(whitePawn, 3, ThreePlayerChessboard.D);
+
+        Bishop leftWhiteBishop = (Bishop) board.getSquare(0, ThreePlayerChessboard.C).getPiece();
+        moves = leftWhiteBishop.allMoves();
+        assertTrue(moves.size() == 5);
+
+        Pawn blackPawn = (Pawn) board.getSquare(6, ThreePlayerChessboard.D).getPiece();
+        board.setPiece(blackPawn, 4, ThreePlayerChessboard.D);
+
+        blackPawn = (Pawn) board.getSquare(6, ThreePlayerChessboard.B).getPiece();
+        board.setPiece(blackPawn, 4, ThreePlayerChessboard.B);
+
+        Bishop rightBlackBishop = (Bishop) board.getSquare(7, ThreePlayerChessboard.C).getPiece();
+        moves = rightBlackBishop.allMoves();
+        assertTrue(moves.size() == 7);
+
+        blackPawn = (Pawn) board.getSquare(6, ThreePlayerChessboard.I).getPiece();
+        board.setPiece(blackPawn, 4, ThreePlayerChessboard.I);
+        blackPawn = (Pawn) board.getSquare(6, ThreePlayerChessboard.K).getPiece();
+        board.setPiece(blackPawn, 4, ThreePlayerChessboard.K);
+
+        Bishop leftBlackBishop = (Bishop) board.getSquare(7, ThreePlayerChessboard.J).getPiece();
+        moves = leftBlackBishop.allMoves();
+        assertTrue(moves.size() == 7);
     }
 }
