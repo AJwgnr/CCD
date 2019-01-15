5fc589e089bf565c3f082b9a31a43ec226e9143f
Fabian Rodriguez <fabianrbz@gmail.com>
2019-01-15 17:29:16 +0100

    Add Javadoc for StraightMoveFactory.

--- src/main/java/com/ovgu/ccd/moves/StraightMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/StraightMoveFactory.java
@@ -6,9 +6,18 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.two.StraightMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * Factory that returns the corresponding straight moves
+ * depending on the board
+ */
 public class StraightMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * @param board current board
+     * @param piece piece to be moved
+     * @return a list of possible moves
+     */
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new com.ovgu.ccd.moves.three.StraightMoves(piece, board);
         } else if (board instanceof Chessboard) {
