9a590ae0f37cade7963afd535e5724304bdc64d6
Fabian Rodriguez <fabianrbz@gmail.com>
2019-01-15 16:24:24 +0100

    Javadoc + styles for castlings.

--- src/main/java/com/ovgu/ccd/applogic/Castling.java
+++ src/main/java/com/ovgu/ccd/applogic/Castling.java
@@ -14,8 +14,8 @@ public class Castling {
     private ThreePlayerChessboard board;
 
     /**
-     * @param king
-     * @param board
+     * @param king to check if in 'check' situation
+     * @param board that's being used
      */
     public Castling(final King king, final ThreePlayerChessboard board) {
         this.king = king;
@@ -23,8 +23,8 @@ public class Castling {
     }
 
     /**
-     * @param piece
-     * @return
+     * @param piece to check if it is a rook
+     * @return true if piece is an instance of Rook
      */
     private boolean isRook(final Piece piece) {
         if (piece == null) {
@@ -40,25 +40,25 @@ public class Castling {
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX X position that king will take
+     * @param posY Y position that king will take
+     * @return the corresponding rook
      */
-    protected Rook getRook(final int x, final int y) {
+    protected Rook getRook(final int posX, final int posY) {
         if (king.getColor() == Player.Colors.WHITE) {
-            if (leftWhiteCastling(x, y)) {
+            if (leftWhiteCastling(posX, posY)) {
                 return (Rook) board.getSquare(0, ThreePlayerChessboard.A).getPiece();
             } else {
                 return (Rook) board.getSquare(0, ThreePlayerChessboard.H).getPiece();
             }
         } else if (king.getColor() == Player.Colors.BLACK) {
-            if (leftBlackCastling(x, y)) {
+            if (leftBlackCastling(posX, posY)) {
                 return (Rook) board.getSquare(7, ThreePlayerChessboard.L).getPiece();
             } else {
                 return (Rook) board.getSquare(7, ThreePlayerChessboard.A).getPiece();
             }
         } else {
-            if (leftGreyCastling(x, y)) {
+            if (leftGreyCastling(posX, posY)) {
                 return (Rook) board.getSquare(11, ThreePlayerChessboard.H).getPiece();
             } else {
                 return (Rook) board.getSquare(11, ThreePlayerChessboard.L).getPiece();
@@ -67,8 +67,8 @@ public class Castling {
     }
 
     /**
-     * @param rook
-     * @return
+     * @param rook that takes part in castling
+     * @return Square in which the rook will be afterwards
      */
     protected Square newRookPosition(final Piece rook) {
         Square pieceSquare = rook.getSquare();
@@ -88,13 +88,13 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if left castling is possible for white king
      */
     public boolean leftWhiteCastlingPossible() {
         Piece piece = null;
 
         if (king.getColor() == Player.Colors.WHITE) {
-            piece = (Piece) board.getSquare(0, ThreePlayerChessboard.A).getPiece();
+            piece = board.getSquare(0, ThreePlayerChessboard.A).getPiece();
         }
         if (!isRook(piece)) {
             return false;
@@ -112,7 +112,7 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if left castling is possible for black king
      */
     public boolean leftBlackCastlingPossible() {
         Piece piece = null;
@@ -136,7 +136,7 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if left castling is possible for grey king
      */
     public boolean leftGreyCastlingPossible() {
         Piece piece = null;
@@ -160,7 +160,7 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if left castling is possible
      */
     public boolean leftCastlingPossible() {
         if (king == null) {
@@ -179,13 +179,13 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if right castling is possible for white king
      */
     public boolean rightWhiteCastlingPossible() {
         Piece piece = null;
 
         if (king.getColor() == Player.Colors.WHITE) {
-            piece = (Piece) board.getSquare(0, ThreePlayerChessboard.H).getPiece();
+            piece = board.getSquare(0, ThreePlayerChessboard.H).getPiece();
         }
         if (!isRook(piece)) {
             return false;
@@ -203,7 +203,7 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if right castling is possible for black king
      */
     public boolean rightBlackCastlingPossible() {
         Piece piece = null;
@@ -227,13 +227,13 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if right castling is possible for grey king
      */
     public boolean rightGreyCastlingPossible() {
         Piece piece = null;
 
         if (king.getColor() == Player.Colors.GREY) {
-            piece = (Piece) board.getSquare(11, ThreePlayerChessboard.L).getPiece();
+            piece = board.getSquare(11, ThreePlayerChessboard.L).getPiece();
         }
         if (!isRook(piece)) {
             return false;
@@ -251,7 +251,7 @@ public class Castling {
     }
 
     /**
-     * @return
+     * @return true if right castling is possible
      */
     public boolean rightCastlingPossible() {
         if (king == null) {
@@ -270,76 +270,76 @@ public class Castling {
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is whit king is castling to the left
      */
-    public boolean leftWhiteCastling(final int x, final int y) {
-        return leftWhiteCastlingPossible() && x == 0 && y == ThreePlayerChessboard.B;
+    public boolean leftWhiteCastling(final int posX, final int posY) {
+        return leftWhiteCastlingPossible() && posX == 0 && posY == ThreePlayerChessboard.B;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is white king is castling to the right
      */
-    private boolean rightWhiteCastling(final int x, final int y) {
-        return rightWhiteCastlingPossible() && x == 0 && y == ThreePlayerChessboard.G;
+    private boolean rightWhiteCastling(final int posX, final int posY) {
+        return rightWhiteCastlingPossible() && posX == 0 && posY == ThreePlayerChessboard.G;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is black king is castling to the left
      */
-    public boolean leftBlackCastling(final int x, final int y) {
-        return leftBlackCastlingPossible() && x == 7 && y == ThreePlayerChessboard.K;
+    public boolean leftBlackCastling(final int posX, final int posY) {
+        return leftBlackCastlingPossible() && posX == 7 && posY == ThreePlayerChessboard.K;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is black king is castling to the right
      */
-    public boolean rightBlackCastling(final int x, final int y) {
-        return rightBlackCastlingPossible() && x == 7 && y == ThreePlayerChessboard.B;
+    public boolean rightBlackCastling(final int posX, final int posY) {
+        return rightBlackCastlingPossible() && posX == 7 && posY == ThreePlayerChessboard.B;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is grey king is castling to the left
      */
-    public boolean leftGreyCastling(final int x, final int y) {
-        return leftGreyCastlingPossible() && x == 11 && y == ThreePlayerChessboard.G;
+    public boolean leftGreyCastling(final int posX, final int posY) {
+        return leftGreyCastlingPossible() && posX == 11 && posY == ThreePlayerChessboard.G;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true is grey king is castling to the right
      */
-    public boolean rightGreyCastling(final int x, final int y) {
-        return rightGreyCastlingPossible() && x == 11 && y == ThreePlayerChessboard.K;
+    public boolean rightGreyCastling(final int posX, final int posY) {
+        return rightGreyCastlingPossible() && posX == 11 && posY == ThreePlayerChessboard.K;
     }
 
     /**
-     * @param x
-     * @param y
-     * @return
+     * @param posX position in which the king will be placed
+     * @param posY position in which the king will be placed
+     * @return true if move is castling
      */
-    public boolean isCastling(final int x, final int y) {
+    public boolean isCastling(final int posX, final int posY) {
         if (king == null) {
             return false;
         }
 
         boolean castling = false;
         if (king.getColor() == Player.Colors.WHITE) {
-            castling = leftWhiteCastling(x, y) || rightWhiteCastling(x, y);
+            castling = leftWhiteCastling(posX, posY) || rightWhiteCastling(posX, posY);
         } else if (king.getColor() == Player.Colors.BLACK) {
-            castling = leftBlackCastling(x, y) || rightBlackCastling(x, y);
+            castling = leftBlackCastling(posX, posY) || rightBlackCastling(posX, posY);
         } else {
-            castling = leftGreyCastling(x, y) || rightGreyCastling(x, y);
+            castling = leftGreyCastling(posX, posY) || rightGreyCastling(posX, posY);
         }
         return castling;
     }
