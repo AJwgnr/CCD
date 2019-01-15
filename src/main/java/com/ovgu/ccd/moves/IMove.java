e0ada34e7094cd041b388d5d6dcdf9edf417436c
Fabian Rodriguez <fabianrbz@gmail.com>
2019-01-15 17:23:40 +0100

    Javadoc for IMove.

--- src/main/java/com/ovgu/ccd/moves/IMove.java
+++ src/main/java/com/ovgu/ccd/moves/IMove.java
@@ -4,6 +4,13 @@ import com.ovgu.ccd.pieces.Square;
 
 import java.util.ArrayList;
 
+/**
+ * Interface for classes that implement moves
+ */
 public interface IMove {
+    /**
+     * @return a list of moves
+     * @throws Exception in case of invalid moves
+     */
     ArrayList<Square> moves() throws Exception;
 }
