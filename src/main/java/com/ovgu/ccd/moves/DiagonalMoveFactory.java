ed5b3dd86ebfdf030eb4a7b0d3045640b55f8389
Fabian Rodriguez <fabianrbz@gmail.com>
2019-01-15 17:22:00 +0100

    JAvadoc + styles for DiagonalMoveFactory.

--- src/main/java/com/ovgu/ccd/moves/DiagonalMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/DiagonalMoveFactory.java
@@ -6,9 +6,18 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.three.DiagonalMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * Factory that returns the corresponding diagonal moves
+ * depending on the board
+ */
 public class DiagonalMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * @param board current board
+     * @param piece to be moved
+     * @return a list of possible diagonal moves
+     */
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new DiagonalMoves(piece, board);
         } else if (board instanceof Chessboard) {
