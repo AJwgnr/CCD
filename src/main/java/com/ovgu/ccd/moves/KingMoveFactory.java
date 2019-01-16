ab564e2ce96c5115787352dd00a18eb98d6701ad
Niha <niha14mohanty@gmail.com>
2019-01-16 03:04:41 +0100

    JavaDocs for KingMoveFactory

--- src/main/java/com/ovgu/ccd/moves/KingMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/KingMoveFactory.java
@@ -6,9 +6,24 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.three.KingMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * This specifically judeges the squares of
+ * the chessboard for the King.
+ */
+
 public class KingMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * This function returns list moves of the
+     * king with respect to the chessboard
+     * i.e. either three or two player.
+     * @param board defines the board on which the
+     *              movement of the King is to determined
+     * @param piece refers to the King of the board
+     * @return the list of squares of the for the King on the given chessboard.
+     */
+
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new KingMoves(piece, board);
         } else if (board instanceof Chessboard) {
