20fa4917908b8efed978f8727159cee785aecbd6
Niha <niha14mohanty@gmail.com>
2019-01-16 02:30:56 +0100

    JavaDocs for DiagonalMoveFactory

--- src/main/java/com/ovgu/ccd/moves/DiagonalMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/DiagonalMoveFactory.java
@@ -6,9 +6,25 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.three.DiagonalMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * Class is used to provide the DiagonalMoves for the pieces
+ * on the chess board whether it is ThreePlayer
+ * or TwoPlayer chessboard by returning the DiagonalMoves
+ * of the respective chessboard.
+ */
+
 public class DiagonalMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     *
+     * @param board defines the type of chessboard currently
+     *              being used
+     * @param piece defines the pieces for which u need
+     *              to decide the move on the given chessbord
+     * @return the DiagonalMoves of a piece on the given ChessBoard
+     */
+
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new DiagonalMoves(piece, board);
         } else if (board instanceof Chessboard) {
