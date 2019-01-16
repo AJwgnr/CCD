b144b749bdfce483267c6f5ec63a7d61c54e3c51
Niha <niha14mohanty@gmail.com>
2019-01-16 03:29:48 +0100

    JavaDocs for StraightMoveFactory

--- src/main/java/com/ovgu/ccd/moves/QueenMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/QueenMoveFactory.java
@@ -6,9 +6,23 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.three.QueenMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * This specifically returns the squares of the
+ * chessboard for the Queen of the game.
+ */
+
 public class QueenMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * Returns the list of the squares of the given
+     * chessboard with respect to the Queen of the game.
+     * @param board defines whether ThreePlayerChessBoard
+     *              or TwoPlayerChess to be provided.
+     * @param piece refers to the Queen of the game.
+     * @return the the list of squares of the given chessboard.
+     */
+
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new QueenMoves(piece, board);
         } else if (board instanceof Chessboard) {
--- src/main/java/com/ovgu/ccd/moves/StraightMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/StraightMoveFactory.java
@@ -6,9 +6,23 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.two.StraightMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * This provides a list of squares of a chessboard
+ * for a piece.
+ */
+
 public class StraightMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * Returns the list of squares for the related
+     * to the straight movement of the pieces on a
+     * given chessboard.
+     * @param board defines the type of chessboard currently being used.
+     * @param piece refers to the pieces of the chess game.
+     * @return the list of squares on a given chessboard with
+     * respect to the given piece specified.
+     */
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new com.ovgu.ccd.moves.three.StraightMoves(piece, board);
         } else if (board instanceof Chessboard) {
