557568092e255546c181d7d4136a5a94ccea0d89
Niha <niha14mohanty@gmail.com>
2019-01-16 02:42:33 +0100

    JavaDocs for IMove interface

--- src/main/java/com/ovgu/ccd/moves/IMove.java
+++ src/main/java/com/ovgu/ccd/moves/IMove.java
@@ -4,6 +4,18 @@ import com.ovgu.ccd.pieces.Square;
 
 import java.util.ArrayList;
 
+/**
+ * This interface returns an ArrayList
+ * of squares of the chessboard.
+ *
+ */
+
 public interface IMove {
+
+    /**
+     * This function returns the list of squares of the moves.
+     * @return an ArrayList of the squares of the chessboard
+     * @throws Exception if certain exception occurs then it throws
+     */
     ArrayList<Square> moves() throws Exception;
 }
