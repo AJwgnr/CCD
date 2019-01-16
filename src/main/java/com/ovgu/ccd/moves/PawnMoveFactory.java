5e11ed3d46406e66f231ea4dfc323efc6c857799
Niha <niha14mohanty@gmail.com>
2019-01-16 03:22:43 +0100

    JavaDocs for PawnMoveFactory

--- src/main/java/com/ovgu/ccd/moves/PawnMoveFactory.java
+++ src/main/java/com/ovgu/ccd/moves/PawnMoveFactory.java
@@ -6,9 +6,24 @@ import com.ovgu.ccd.gui.Chessboard;
 import com.ovgu.ccd.moves.three.PawnMoves;
 import com.ovgu.ccd.pieces.Piece;
 
+/**
+ * This helps in getting the squares of a given
+ * chessboard, whether Three player or Two Player,
+ * related to the Pawns of the chess game.
+ */
+
 public class PawnMoveFactory {
 
-    public static IMove getMoves(IBoard board, Piece piece) {
+    /**
+     * Returns the list of the squares of the specified
+     * chessboard with respect to the Pawns of the chess game.
+     * @param board defines the type of chessboard,
+     *              i.e. Two or Three player's ones
+     * @param piece refers to the Pawns of the chess game
+     * @return list of squares of the chessboard for the Pawns.
+     */
+
+    public static IMove getMoves(final IBoard board, final Piece piece) {
         if (board instanceof ThreePlayerChessboard) {
             return new PawnMoves(piece, board);
         } else if (board instanceof Chessboard) {
